0259c4e8384a8a57fbd563c5a7d6b76a6e0cf89f
shahfahad298 <shahfahad298@gmail.com>
2017-10-20 15:34:10 -0300

    first commit

---
+++ task2.py
@@ -0,0 +1,19 @@
+
+import requests
+from weather import weather
+weather = Weather()
+
+
+class Weather(object):
+    URL = 'http://yahoo.com/weather'
+
+    def lookup(self, woeid):
+        url = "https://yahoo.com/weather/"
+	return results
+    def condition(self, cond):
+        url = "https://yahoo.com/weather"
+        return results
+print (condition(""))
+
+for forcast in condition:
+	print()  
